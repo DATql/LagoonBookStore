6214db5e1cbb4b7a1b22f67b7d2c5321ed284fec
DATql <goneanhem@gmail.com>
2023-03-17 15:40:56 +0700

    main

--- WebLagoonBookStore/Controllers/HomeController.cs
+++ WebLagoonBookStore/Controllers/HomeController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
-namespace WebLagoonBookStore.Controllers
+using WebLagoonBookStore.Models;
+namespace WebLagoonBookStore
 {
     public class HomeController : Controller
-    {
-        // GET: Home
+    {   
+        private  BookStoreContextDataContext db = new BookStoreContextDataContext();
+
         public ActionResult Index()
         {
-            return View();
+            var result = from b in db.Books
+                         join bt in db.Book_Tags on b.id equals bt.bookID
+                         join t in db.Tags on bt.tagID equals t.id
+                         where t.tag1 =="best seller"
+                         orderby t.tag1
+                         select b
+                        ;
+            return View(result);
+        }
+
+        [HttpPost]
+        public ActionResult AddToCart(int productId, int quantity)
+        {
+            // Lưu thông tin sản phẩm vào biến tạm
+            var cart = Session["Cart"] as List<ReceiptDetail> ?? new List<ReceiptDetail>();
+            var book = db.Books.Where(p=>p.id ==productId);
+            cart.Add(new ReceiptDetail {bookid = book, Quantity = quantity });
+            Session["Cart"] = cart;
+
+            // Chuyển hướng đến trang Order
+            return RedirectToAction("Index", "Order");
         }
     }
 }
